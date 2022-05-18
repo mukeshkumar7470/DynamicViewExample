5e60069dd0174d1fb29f6517d0828b1dd180074b
kumarmukeshpatel57 <kumarmukeshpatel57@gmail.com>
2022-05-18 17:52:13 +0530

    Working

--- app/src/main/java/com/mukeshkpdeveloper/dynamicviewexample/MainActivity.java
+++ app/src/main/java/com/mukeshkpdeveloper/dynamicviewexample/MainActivity.java
@@ -18,12 +18,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private static RecyclerView recyclerView;
     private static ArrayList<DataModel> data;
-    ItemClickListener itemClickListener;
-    Context mContext;
     int count = 0;
 
     @Override
@@ -33,44 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         data = new ArrayList<DataModel>();
 
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        recyclerView.setHasFixedSize(true);
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        // Initialize listener
-        itemClickListener= new ItemClickListener() {
-            @Override
-            public void onClick(int position, String value, String flag) {
-
-                if (flag.equalsIgnoreCase("add")) {
-                    count++;
-                    DataModel dataModel = new DataModel(
-                            "Test", ""+count, R.drawable.ic_launcher_background, 0
-                    );
-                    data.add(dataModel);
-                    Toast.makeText(getApplicationContext(),"Position : "
-                            +position +" || Value : "+dataModel.getVersion(),Toast.LENGTH_SHORT).show();
-                } else {
-                    count--;
-                    if (data.size() > 1) {
-                        data.remove(position);
-                    }
-                    Toast.makeText(getApplicationContext(),"Position : "
-                            +position +" || Value : "+value,Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-
-        DataModel dataModel = new DataModel(
-                "Test", "1", R.drawable.ic_launcher_background, 0
-        );
-        data.add(dataModel);
-
-        adapter = new CustomAdapter(data, mContext, itemClickListener);
-        recyclerView.setAdapter(adapter);
 
         AppCompatButton btnCheckData = findViewById(R.id.btnCheckData);
         btnCheckData.setOnClickListener(new View.OnClickListener() {
